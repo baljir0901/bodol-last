aa4fe1aafdf92a0ea5c61622fb02dbab42535e96
Baljir0901 <baljir0901@gmail.com>
2025-07-26 16:12:02 +0900

    Update: MongoDB cluster, JWT secret, CORS config

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,18 @@ const userRouter = require('./Routes/userRoutes');
 const postRouter = require('./Routes/postRoutes');
 const replyRouter = require('./Routes/replyRoute');
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      'https://bodolspace-frontend-4aw3wxg4-baljirs-projects.vercel.app',
+      'http://localhost:3000',
+      'http://localhost:5173',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
